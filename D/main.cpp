66d789db4636affc98784a169ea2c18a2b8ba62c
gramdel <legovec96@gmail.com>
2022-02-28 20:44:46 +0300

    solved D

--- D/main.cpp
+++ D/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a;
+    short b, c, d;
+    long long k;
+    std::cin >> a >> b >> c >> d >> k;
+
+    for (long long i = 1; i <= k; i++) {
+        if (a >= (c + d) / b) {
+            a = d;
+            break;
+        }
+        if (a <= c / b) {
+            a = 0;
+            break;
+        }
+        a = a * b - c;
+        if (b == double(c) / a + 1) {
+            break;
+        }
+    }
+    std::cout << a;
     return 0;
 }
