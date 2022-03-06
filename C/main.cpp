e8619a7c323bd5af7245c7332292a1fb9c406078
GramDEL <legovec96@gmail.com>
2022-03-06 22:39:16 +0300

    Added solution for C (TIME LIMIT)

--- C/main.cpp
+++ C/main.cpp
@@ -1,41 +1,61 @@
 #include <iostream>
 #include <string>
+#include <stack>
 #include <map>
 #include <regex>
 
 using namespace std;
 
-void parse(string s, map<string, int> map_of_vars) {
-    if (s[0] == '{') {
-        return;
-        //next call
-    } else if (s[0] == '}') {
-        return;
-        //next call
-    } else {
-        int middle = (int) s.find('=');
-        string left = s.substr(0, middle);
-        string right = s.substr(middle + 1, s.length() - middle-1);
-
-        cout << left << " " << right << endl;
-
-        regex exp("\\d+");
-        if (regex_match(right, exp)) {
-            cout << "MATCH\n";
-        }
-    }
-}
-
 int main() {
-    map<string, int> map_of_vars;
-
+    map<string, stack<int> *> map_of_vars;
     string s;
 
     while (getline(cin, s)) {
         if (s.empty()) {
             break;
+        } else if (s[0] == '{') {
+            for (auto &pair: map_of_vars) {
+                pair.second->push(pair.second->top());
+            }
+        } else if (s[0] == '}') {
+            for (auto &pair: map_of_vars) {
+                if (pair.second->size() == 1) {
+                    pair.second->top() = 0;
+                } else {
+                    pair.second->pop();
+                }
+            }
+        } else {
+            int middle = (int) s.find('=');
+            string left = s.substr(0, middle);
+            string right = s.substr(middle + 1, s.length() - middle - 1);
+
+            if (map_of_vars.find(left) == map_of_vars.end()) {
+                map_of_vars[left] = new stack<int>;
+                map_of_vars[left]->push(0);
+            }
+
+            if (left == right) {
+                cout << map_of_vars[left]->top() << endl;
+                continue;
+            }
+
+            regex exp("-?\\d+");
+            if (regex_match(right, exp)) {
+                map_of_vars[left]->top() = stoi(right);
+            } else {
+                if (map_of_vars.find(right) == map_of_vars.end()) {
+                    map_of_vars[right] = new stack<int>;
+                    map_of_vars[right]->push(0);
+                }
+                map_of_vars[left]->top() = map_of_vars[right]->top();
+                cout << map_of_vars[right]->top() << endl;
+            }
         }
-        parse(s, map_of_vars);
+    }
+
+    for (auto &pair: map_of_vars) {
+        delete pair.second;
     }
 
     return 0;
