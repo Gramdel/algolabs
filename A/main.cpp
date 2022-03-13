56f0f760ad4367809890c6c151b8dca71f45788f
gramdel <legovec96@gmail.com>
2022-03-13 13:32:34 +0300

    Deleted unnecessary comments

--- A/main.cpp
+++ A/main.cpp
@@ -4,11 +4,9 @@ using namespace std;
 
 int main() {
     int n;
-    //cout << "Enter n:\n";
     cin >> n;
 
     int *a = new int[n];
-    //cout << "Enter " << n << " values:\n";
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
