747a96580787842cefa9e68c09c46d62f3c9d913
gramdel <legovec96@gmail.com>
2022-03-20 17:31:57 +0300

    Added solution for F

--- F/main.cpp
+++ F/main.cpp
@@ -1,37 +1,25 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <list>
 
 using namespace std;
 
-string choose_greater(string a, string b) {
-    if (a.empty()) {
-        return "";
-    } else if (a[0] == b[0]) {
-        return a[0] + choose_greater(a.substr(1, a.length() - 1), b.substr(1, b.length() - 1));
-    } else if (a[0] > b[0]) {
-        return a;
-    } else {
-        return b;
-    }
+bool choose_greater(const string &a, const string &b) {
+    return a + b > b + a;
 }
 
 int main() {
-    short n;
-    cin >> n;
+    list<string> chunks;
+    string tmp;
 
-    vector<string> chunks;
-    for (int i = 0; i < n; i++) {
-        string tmp;
-        cin >> tmp;
-        chunks.push_back(tmp);
+    while (cin >> tmp) {
+        chunks.push_front(tmp);
     }
+    chunks.sort(choose_greater);
 
-    cout << choose_greater("123", "124") << endl;
-    cout << choose_greater("123", "133") << endl;
-    cout << choose_greater("3", "2") << endl;
-    cout << choose_greater("123", "223") << endl;
-    cout << choose_greater("1", "1");
+    for (const auto &el: chunks) {
+        cout << el;
+    }
 
     return 0;
 }
