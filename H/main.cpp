60fc8992c512905ba616b44ebe2ec8849ace0526
gramdel <legovec96@gmail.com>
2022-04-04 18:55:31 +0300

    Problem H, first try

---
+++ H/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> prices(n);
+    for (auto &el: prices) {
+        cin >> el;
+    }
+    sort(prices.rbegin(), prices.rend());
+
+    int count = 1;
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        if (n >= k) {
+            if (count == k) {
+                count = 0;
+                continue;
+            } else {
+                count++;
+            }
+        }
+        result+=prices[i];
+    }
+    cout << result;
+
+    return 0;
+}
