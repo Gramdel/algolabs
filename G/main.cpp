77467a0450243655fb09e2f4e7027f41216f72e5
gramdel <legovec96@gmail.com>
2022-04-07 16:33:29 +0300

    Solved G

--- G/main.cpp
+++ G/main.cpp
@@ -8,36 +8,37 @@ int counters[26];
 int weights[26];
 
 bool compare(char a, char b) {
-    if (counters[a - 97] == 1) {
-        return counters[b - 97] != 1;
-    }
-    if (counters[b - 97] == 1) {
-        return false;
-    }
     return weights[a - 97] < weights[b - 97];
 }
 
 int main() {
-    string word;
+    string word, new_word;
     cin >> word;
+
     std::fill(counters, counters + 26, 0);
     for (auto &el: word) {
         counters[el - 97]++;
     }
-    for (auto &el: weights) {
-        cin >> el;
-    }
-    sort(word.rbegin(), word.rend(), compare);
 
-    for (int i = word.size() - 1; i >= 0; ) {
-        if (counters[word[i] - 97] != 1) {
-            word.push_back(word[i]);
-            word.erase(word.begin() + i);
-            i--;
+    for (int i = 0; i < 26; i++) {
+        cin >> weights[i];
+        if (weights[i] && counters[i] > 1) {
+            new_word += (char) (97 + i);
+            counters[i] -= 2;
         }
-        i-=counters[word[i] - 97];
     }
-    cout << word;
+    sort(new_word.rbegin(), new_word.rend(), compare);
+
+    for (int i = 0; i < 26; i++) {
+        for (int j = 0; j < counters[i]; j++) {
+            new_word += (char) (97 + i);
+        }
+    }
+
+    for (int i = word.length() - new_word.length() - 1; i >= 0; i--) {
+        new_word += new_word[i];
+    }
+    cout << new_word;
 
     return 0;
 }
