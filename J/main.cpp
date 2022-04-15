78cb3e6cbc1586687438368c714452a5a3be3152
gramdel <legovec96@gmail.com>
2022-04-15 13:22:36 +0300

    Solved J

--- J/main.cpp
+++ J/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <list>
+
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
     cin.ignore();
-    vector<string> queue;
+    list<string> left;
+    list<string> right;
     for (int i = 0; i < n; i++) {
         string tmp;
         getline(cin, tmp);
         if (tmp[0] == '+') {
-            queue.push_back(tmp.substr(2,tmp.length()-2));
+            right.push_back(tmp.substr(2, tmp.length() - 2));
         } else if (tmp[0] == '*') {
-            queue.insert(queue.end()-queue.size()/2, tmp.substr(2,tmp.length()-2));
+            right.push_front(tmp.substr(2, tmp.length() - 2));
         } else {
-            cout << queue[0] << endl;
-            queue.erase(queue.begin());
+            cout << left.front() << endl;
+            left.pop_front();
+        }
+        if (left.size() < right.size()) {
+            left.push_back(right.front());
+            right.pop_front();
         }
     }
 
