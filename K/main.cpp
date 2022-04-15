116d08e80b56ef3f501ae3e4f0af5c3d64dd4617
gramdel <legovec96@gmail.com>
2022-04-15 13:40:01 +0300

    Initial commit for K

---
+++ K/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int tmp;
+        cin >> tmp;
+    }
+
+    return 0;
+}
